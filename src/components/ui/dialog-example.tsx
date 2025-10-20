85f219f41435798ef87f3e27b67458527b68edec
[dyad] <hengkisetiawan461@gmail.com>
2025-10-20 17:45:45 +0800

    [dyad] wrote 1 file(s)

---
+++ src/components/ui/dialog-example.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
+export function AccessibleDialog() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Open Dialog</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Dialog Title</DialogTitle>
+          <DialogDescription>
+            This is a description of the dialog content.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="py-4">
+          <p>Dialog content goes here.</p>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
